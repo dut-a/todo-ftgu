9c3daee3df21cb73868df172c95adf09ccb9e3bd
dut-a <d.dut@live.com>
2020-06-13 21:10:27 -0500

    Adds support for Redux Dev Tools

--- src/store.js
+++ src/store.js
@@ -5,16 +5,21 @@ import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import { todos } from './todos/reducers';
 
 const reducers = {
-    todos,
+  todos,
 };
 
 const persistConfig = {
-    key: 'root',
-    storage,
-    stateReconciler: autoMergeLevel2,
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2,
 }
 
 const rootReducer = combineReducers(reducers);
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const configureStore = () => createStore(persistedReducer);
+export const configureStore = () =>
+  createStore(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+
